c08345d36dc6406c42302815fbc8ec5d8141a790
DrBoltzmann <mark@idezo.ch>
2018-08-08 09:28:09 +0000

    Modified Kalman predict funtion

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -76,23 +76,26 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
       * Create the covariance matrix.
       * Remember: you'll need to convert radar from polar to cartesian coordinates.
     */
+    
     // first measurement
     cout << "EKF: " << endl;
     ekf_.x_ = VectorXd(4);
     ekf_.x_ << 1, 1, 1, 1;
 
     if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
-      /**
-      Convert radar from polar to cartesian coordinates and initialize state.
-      */
+      // Convert radar from polar (theta, r) to cartesian (x, y) coordinates and initialize state.
+      ekf_.x_(0) << ro*cos(theta);
+      ekf_.x_(1) << ro*sin(theta);
     }
     else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
-      /**
-      Initialize state.
-      */
+      // Initialize the state
+      ekf_.x_(0) << x;
+      ekf_.x_(1) << y;
     }
 
     // done initializing, no need to predict or update
+    // ekf_.F << st to 1 diagonal matrix
+    //previous timestamp = measurement_pack.timestamp;
     is_initialized_ = true;
     return;
   }
@@ -107,8 +110,27 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
       - Time is measured in seconds.
      * Update the process noise covariance matrix.
      * Use noise_ax = 9 and noise_ay = 9 for your Q matrix.
-   */
-
+   */ 
+  
+  // define dt in seconds
+  float dt = (measurement_pack.timestamp_ - previous_timestamp_) / 1000000.0;
+  previous_timestamp_ = measurement_pack.timestamp_;
+  
+  float dt_2 = dt * dt;
+  float dt_3 = dt_2 * dt;
+  float dt_4 = dt_3 * dt;
+  
+  // Modify the F matrix to integrate the time
+  ekf_.F_(0, 2) = dt;
+  ekf_.F_(1, 3) = dt;
+  
+  // Set the process covariance matrix Z
+  ekf_.Q_ = MatrixXd(4, 4);
+  ekf_.Q_ << dt_4/4*noise_ax, 0, dt_3/2*noise_ax, 0,
+             0, dt_4/4*noise_ay, 0, dt_3/2*noise_ay, 0,
+             dt_3/2*noise_ax, 0, dt_2*noise_ax, 0,
+             0, dt_3/2*noise_ay, 0, dt_2*noise_ay;
+    
   ekf_.Predict();
 
   /*****************************************************************************
--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -12,26 +12,34 @@ KalmanFilter::~KalmanFilter() {}
 
 void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
                         MatrixXd &H_in, MatrixXd &R_in, MatrixXd &Q_in) {
-  x_ = x_in;
-  P_ = P_in;
-  F_ = F_in;
-  H_ = H_in;
-  R_ = R_in;
-  Q_ = Q_in;
+  x_ = x_in; // object state
+  P_ = P_in; // object covariance matrix
+  F_ = F_in; // state transition matrix
+  H_ = H_in; // measurement matrix
+  R_ = R_in; // measurement covariance matrix
+  Q_ = Q_in; // process covariance matrix
 }
 
 void KalmanFilter::Predict() {
-  /**
-  TODO:
-    * predict the state
-  */
+  // predict the state
+  //x = F * x + u; where u is external motion
+  x = F_ * x_;
+  MatrixXd Ft = F_.transpose();
+  P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
-  /**
-  TODO:
-    * update the state by using Kalman Filter equations
-  */
+  // update the state by using Kalman Filter equations
+  VectorXd y = z - H * x;
+  MatrixXd Ht = H.transpose();
+  MatrixXd S = H * P * Ht + R;
+  MatrixXd Si = S.inverse();
+  MatrixXd K =  P * Ht * Si;
+  
+  // New state
+  x = x + (K * y);
+  P = (I - K * H) * P;
+  
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
